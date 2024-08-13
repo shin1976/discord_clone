40f73f43f2541522597e91c99c34283c57d7ac34
shin1976 <info@shinito.net>
2024-08-14 04:24:36 +0900

    mac

---
+++ discode-clone-udemy/src/components/sidebar/SidebarChannel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./SidebarChannel.scss";
+
+export default function SidebarChannel() {
+  return <div className="sidebarChannel">
+    <h4>
+        <span className='sidebarChannelHash'>#</span>
+        Udemy
+    </h4>
+  </div>
+};
